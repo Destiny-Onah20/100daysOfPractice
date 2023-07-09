23fb1bb3e99e2024baca2008444be83ab915f733
Destiny <onahdestinyc20@gmail.com>
2023-07-09 02:29:33 +0100

    made update on firebase

--- dist/controllers/products.controller.js
+++ dist/controllers/products.controller.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,9 +36,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createProducts = void 0;
-const cloudinary_1 = __importDefault(require("../middlewares/cloudinary"));
 const products_model_1 = __importDefault(require("../models/products.model"));
 const user_model_1 = __importDefault(require("../models/user.model"));
+const admin = __importStar(require("firebase-admin"));
+const firebase_admin_1 = require("firebase-admin");
+const firebase_1 = require("../utils/firebase");
 const createProducts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     try {
@@ -28,22 +53,42 @@ const createProducts = (req, res) => __awaiter(void 0, void 0, void 0, function*
                 message: 'User not found!'
             });
         }
+        try {
+            admin.initializeApp({
+                credential: admin.credential.cert(firebase_1.serviceAccount),
+                storageBucket: process.env.STORE_BUCK,
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
         if (!req.files) {
             throw new Error('No file uploaded');
         }
+        const bucket = (0, firebase_admin_1.storage)().bucket();
+        const initFirestore = (0, firebase_admin_1.firestore)();
         const file = (_a = req.files) === null || _a === void 0 ? void 0 : _a.imageId;
         if (!file || file.length === 0) {
             throw new Error('No file uploaded');
         }
         const uploads = Array.isArray(file) ? file : [file];
         for (const file of uploads) {
-            const result = yield cloudinary_1.default.uploader.upload(file.tempFilePath);
+            // const result = await Cloudinary.uploader.upload(file.tempFilePath);
+            const uploadPath = `uploads/${file.tempFilePath}`;
+            const fileRef = bucket.file(uploadPath);
+            // Upload the file to Firebase Cloud Storage
+            const snapshot = yield fileRef.save(file.data, {
+                metadata: {
+                    contentType: file.mimetype,
+                },
+            });
+            const url = `https://storage.googleapis.com/${bucket.name}/${uploadPath}`;
             const data = {
                 productName,
                 description,
                 price,
-                imageId: result.secure_url,
-                cloudId: result.public_id,
+                imageId: url,
+                cloudId: "result.public_id",
                 userId: Number(userId)
             };
             const postProduct = yield products_model_1.default.create(data);
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -4,8 +4,10 @@ import { productDataInterface } from '../interfaces/product.interface';
 import Product from '../models/products.model';
 import { UploadedFile } from 'express-fileupload';
 import User from '../models/user.model';
-import { v4 as uuidv4 } from 'uuid';
-import fs from 'fs';
+import * as admin from "firebase-admin";
+import { ServiceAccount, firestore, storage, initializeApp } from "firebase-admin";
+import { serviceAccount, firebaseConfig } from '../utils/firebase';
+
 
 export const createProducts = async (req: Request, res: Response) => {
   try {
@@ -19,18 +21,38 @@ export const createProducts = async (req: Request, res: Response) => {
         message: 'User not found!'
       })
     }
+    try {
+      admin.initializeApp({
+        credential: admin.credential.cert((serviceAccount as ServiceAccount)),
+        storageBucket: process.env.STORE_BUCK,
+      });
+    } catch (error: any) {
+      console.log(error);
+    }
     if (!req.files) {
       throw new Error('No file uploaded');
     }
-
+    const bucket = storage().bucket();
+    const initFirestore = firestore()
     const file = req.files?.imageId as UploadedFile[];
     if (!file || file.length === 0) {
       throw new Error('No file uploaded');
     }
     const uploads = Array.isArray(file) ? file : [file]
     for (const file of uploads) {
-      const result = await Cloudinary.uploader.upload(file.tempFilePath);
+      // const result = await Cloudinary.uploader.upload(file.tempFilePath);
+      const uploadPath = `uploads/${file.tempFilePath}`;
+      const fileRef = bucket.file(uploadPath);
 
+      // Upload the file to Firebase Cloud Storage
+      const snapshot = await fileRef.save(file.data, {
+        metadata: {
+          contentType: file.mimetype,
+        },
+      })
+
+      const url = `https://storage.googleapis.com/${bucket.name}/${uploadPath}`;
+      // const theUrl = await getDownloadURL(ref(getStorage()))
       interface productDataInterface {
         productName: string;
         description: string;
@@ -43,8 +65,8 @@ export const createProducts = async (req: Request, res: Response) => {
         productName,
         description,
         price,
-        imageId: result.secure_url,
-        cloudId: result.public_id,
+        imageId: url,
+        cloudId: "result.public_id",
         userId: Number(userId)
       };
       const postProduct = await Product.create(data);
